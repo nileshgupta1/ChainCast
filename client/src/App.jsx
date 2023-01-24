1a41a4d69908ddd461a694aae81dd6eb6cae4830
Nilesh <nilesh147k@gmail.com>
2023-01-24 23:54:55 +0530

    render navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <h1 class="text-3xl font-bold underline">
         Hello world!
       </h1>
