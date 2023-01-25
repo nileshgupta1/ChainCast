9f9374f301d6b08b9bbb33d53c36018a8683109a
Nilesh <nilesh147k@gmail.com>
2023-01-26 03:25:43 +0530

    update gradients

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,9 +7,9 @@ function App() {
   const post = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
   return (
     <div className='bg-body h-screen w-screen m-0'>
-      <Navbar/>
-      <SideBar/>
-      <Post userName="Nilu-Chan" postContent = {post}/>
+      <Navbar />
+      <SideBar />
+      <Post userName="Nilu-Chan" postContent={post} />
     </div>
   )
 }
