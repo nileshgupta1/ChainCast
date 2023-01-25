08145542d05b2052808a6540de9b38d2f5b2ba53
Devendra Tarak <dekuta75@gmail.com>
2023-01-26 02:31:31 +0530

    Modified the theme to be in gradient

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import './App.css'
 import Navbar from './components/Navbar'
+import SideBar from './components/SideBar'
+import Post from './components/Post'
 
 function App() {
-
+  const post = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
   return (
-    <div>
-      <Navbar />
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className='bg-body h-screen w-screen m-0'>
+      <Navbar/>
+      <SideBar/>
+      <Post userName="Nilu-Chan" postContent = {post}/>
     </div>
   )
 }
